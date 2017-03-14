cba416d060ca9c09954be4a96a295c40ad49e44c
craj1031tx <craj1031tx@gmail.com>
2017-03-14 00:08:13 -0500

    final commit for 3/13/17

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            string testString = "Irving, TX";
+            int testNum = 12;
+            string testInterpolate = $"interpolate this plz: {3*3}";
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test. {0} {1}, age {2} and lives in {3}", "Chetan", "Raj", 27, testString);
+            Console.WriteLine("Here is a test"+testNum+"of some commas");
+            Console.WriteLine($"Here is some math: {5+5}");
+            Console.WriteLine(testInterpolate);
         }
     }
 }
