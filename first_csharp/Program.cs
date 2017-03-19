d493f1e19ca78ebf9aa2ca10049d33ea58cbd31c
craj1031tx <craj1031tx@gmail.com>
2017-03-18 23:32:56 -0500

    saturday work 3/18/17

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -6,14 +7,49 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            string testString = "Irving, TX";
-            int testNum = 12;
-            string testInterpolate = $"interpolate this plz: {3*3}";
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This is a test. {0} {1}, age {2} and lives in {3}", "Chetan", "Raj", 27, testString);
-            Console.WriteLine("Here is a test"+testNum+"of some commas");
-            Console.WriteLine($"Here is some math: {5+5}");
-            Console.WriteLine(testInterpolate);
+            Random rand = new Random();
+            int[] numArray = {0,1,2,3,4,5,6,7,8,9};
+            string[] names = {"Tim", "Martin", "Nikki", "Sara"};
+            bool[] truths = new bool[10];
+            for(int i =0;i<truths.Length;i++){
+                if(i%2==0){
+                    truths[i] = true;
+                }
+            }
+            int[,] multTable = new int[10,10];
+            for(int first = 0;first<10;first++){
+                for(int second=0;second<10;second++){
+                    multTable[first,second] = first*second;
+                }
+            }
+            Console.WriteLine(multTable);
+            foreach(var item in multTable){
+                Console.WriteLine(item);
+            }
+            List<string> iceCream = new List<string>();
+            iceCream.Add("Chocolate");
+            iceCream.Add("Vanilla");
+            iceCream.Add("Cookies and Cream");
+            iceCream.Add("Strawberry");
+            iceCream.Add("Birthday Cake");
+
+            foreach (string flavor in iceCream){
+                Console.WriteLine(flavor);
+            }
+            Console.WriteLine("count is" + iceCream.Count);
+            Console.WriteLine(iceCream[2]);
+            iceCream.RemoveAt(2);
+            Console.WriteLine(iceCream.Count);
+
+            Dictionary<string,string> userInfo = new Dictionary<string,string>();
+            foreach(string userName in names){
+                userInfo.Add(userName, null);
+            }
+
+            foreach(var entry in userInfo){
+                //Console.WriteLine(entry);
+                userInfo[entry.Value] = iceCream[rand.Next(0,4)];
+            }
         }
     }
 }
